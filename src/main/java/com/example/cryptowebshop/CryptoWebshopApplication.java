d115152bebf51003df514b6b4fa85391ed39ccea
Raffiq Iblila <Raffiq.Iblila@netcetera.com>
2024-07-19 09:55:02 +0200

    [NCA225-0000] created the project

---
+++ src/main/java/com/example/cryptowebshop/CryptoWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptowebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cryptowebshop/CryptoWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptowebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
